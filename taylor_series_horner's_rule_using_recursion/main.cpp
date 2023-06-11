e0043ab19580dd9506159cfbdd5926a9e18cd746
ubyhzargam <ubyhzargam@gmail.com>
2023-06-11 11:54:56 +0530

    Initial Commit

---
+++ taylor_series_horner's_rule_using_recursion/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  taylor_series_horner's_rule_using_recursion
+//
+//  Created by Uby H on 11/06/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
