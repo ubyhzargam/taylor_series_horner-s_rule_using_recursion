0359d53c5b9107abb363211920bc2374445afbd3
ubyhzargam <ubyhzargam@gmail.com>
2023-06-14 15:39:26 +0530

    Update main.cpp
    This code finds the Taylor series by taking the input from the user. It uses Horner's rule and thus reduces the number of multiplications and thus reducing the time complexity of the code. Using iterative loops would be more efficient as there are less number of activation records created. This code shows how to use recursive function to find Taylor's series by using Horner's rule.

--- taylor_series_horner's_rule_using_recursion/main.cpp
+++ taylor_series_horner's_rule_using_recursion/main.cpp
@@ -6,9 +6,22 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+double taylor(int x, int n)
+{
+    static double s=1;
+    if(n==0)
+        return s;
+    s=1+x*s/n;
+    return taylor(x,n-1);
+}
+    int main()
+{
+    int x,n;
+    cout<<"Enter x to find taylor series : ";
+    cin>>x;
+    cout<<"Enter n : ";
+    cin>>n;
+    cout<<"The result = "<<taylor(x,n)<<endl;
     return 0;
 }
